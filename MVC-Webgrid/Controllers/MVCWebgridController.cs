0d7fc8b9778ea08aebda0c4a0683e61a0596a48d
mojde7th <mojde7th@gmail.com>
2022-03-05 10:44:53 +0330

    Add project files.

---
+++ MVC-Webgrid/Controllers/MVCWebgridController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Webgrid.Controllers
+{
+    public class MVCWebgridController : Controller
+    {
+        // GET: MVCWebgrid
+        public ActionResult Index()
+        {
+            SampleDBEntities sd = new SampleDBEntities();
+            return View(sd.users.ToList());
+        }
+    }
+}
---
+++ MVC-Webgrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Webgrid.Startup))]
+namespace MVC_Webgrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
